496605d97c40a721df56da4d7f0b947a8b1ed1aa
Secrethan <107679440+Secrethan@users.noreply.github.com>
2022-11-27 22:02:51 +0900

    [Bronze III] Title: 알람 시계, Time: 236 ms, Memory: 18480 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {		
+		Scanner input = new Scanner(System.in);
+			int hour = input.nextInt();
+			int minute = input.nextInt();
+			if((hour >=0 && hour <= 23) && (minute >=0 && minute <=59)) {
+			
+			if((hour == 0)) {
+				
+				if(minute >=45) {
+					minute = minute - 45;
+				}
+				else {
+					hour = 23;
+					minute = 60 - (45 - minute);
+				}
+			}
+			//hour = 1~ 23 일때
+			else {
+					
+					if(minute >=45) {
+						minute = minute - 45;
+					}
+					else {
+						hour = hour -1;
+						minute = 60 - (45 - minute);
+					}
+			}
+			
+			}
+			System.out.println(hour+" "+minute);
+			
+			input.close();
+	
+	}
+}
+

f7b0a47170dc1c1dd16798e76c5f77cba972f85a
Secrethan <107679440+Secrethan@users.noreply.github.com>
2023-01-21 16:12:04 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.08 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+	        
+	        StringBuffer sb = new StringBuffer(my_string);
+	        String reverse = sb.reverse().toString();
+	        
+	        answer = reverse;
+	        
+	        
+	        return answer;
+    }
+}

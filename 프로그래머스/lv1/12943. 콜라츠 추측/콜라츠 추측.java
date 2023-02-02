32a1cc6edbdb74eb24e6da74588b67b7a4d25bd3
Secrethan <107679440+Secrethan@users.noreply.github.com>
2023-02-02 22:01:39 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,16 +1,10 @@
 class Solution {
 	public int solution(long num) {
-		int answer = 0;
-	
-		while(num != 1) {
-			num = num % 2 == 0 ? (num / 2) : (num * 3 + 1);
-			answer++;
-			if (answer == 500) {
-				answer = -1;
-				break;
-			}
+		for (int i = 0; i < 500; i++) {
+			if (num == 1)
+				return i;
+			num = (num % 2 == 0) ? (num / 2) : (num * 3 + 1);
 		}
-	
-		return answer;
+		return -1;
 	}
 }

48d6d4962e67cb5310ce3c2341fcff60a13a10d3
Secrethan <107679440+Secrethan@users.noreply.github.com>
2023-02-21 21:54:15 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 572 ms, Memory: 25936 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main{
+	
+    static int result;
+    public static int recursion(String s, int l, int r){
+    	result++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+    
+    public static int isPalindrome(String s){
+        return recursion(s, 0, s.length()-1);
+    }
+    
+    public static void main(String[] args){
+    	Scanner input = new Scanner(System.in);
+    	int t = input.nextInt();
+    	for(int i = 0; i < t; i++) {
+    		result = 0;
+    		System.out.println(isPalindrome(input.next()) + " " + result);
+    	}
+    	input.close();
+    }
+}

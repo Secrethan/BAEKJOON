31bf3218712f6a351b211b5421c6f9646041ff18
Secrethan <107679440+Secrethan@users.noreply.github.com>
2023-08-16 00:40:31 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 668 ms, Memory: 66800 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -3,21 +3,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-
+        String answer ="";
+        String str[] = new String[n];
         for (int i = 0; i < n; i++) {
-            String[] words = br.readLine().split(" ");
-
-            for (String word : words) {
-                StringBuilder sb = new StringBuilder(word);
-                System.out.print(sb.reverse().toString() + " ");
-            }
-            System.out.println();
+            str[i] = br.readLine();
         }
         br.close();
+
+        for(String x : str) {
+            sb.setLength(0);
+            int pos;
+            while((pos = x.indexOf(" ")) != -1) {
+                String tmp = x.substring(0,pos);
+                sb.append(new StringBuilder(tmp).reverse().toString()+" ");
+                x = x.substring(pos+1);
+            }
+            sb.append(new StringBuilder(x).reverse().toString());
+            System.out.println(sb);
+        }
     }
 }
 

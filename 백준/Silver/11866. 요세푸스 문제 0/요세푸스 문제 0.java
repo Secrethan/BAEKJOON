d1cdb82f487e7983023c6440b9336f69ce330b7c
Secrethan <107679440+Secrethan@users.noreply.github.com>
2023-08-30 21:16:02 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 164 ms, Memory: 22832 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public String solution(int n, int k) {
+        Queue<Integer> q = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            q.add(i);
+        }
+        sb.append("<");
+
+        while (q.size() > 1) {
+            for (int i = 0; i < k - 1; i++) {
+                q.offer(q.poll());
+            }
+            sb.append(q.poll()).append(", ");
+        }
+        sb.append(q.poll()).append(">");
+        String answer = sb.toString();
+        return answer;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        System.out.println(m.solution(n, k));
+    }
+}

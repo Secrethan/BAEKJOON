d3f0bdcd9343db5af195af44e0df329a94b1267f
Secrethan <107679440+Secrethan@users.noreply.github.com>
2023-09-03 23:05:30 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 168 ms, Memory: 16280 KB -BaekjoonHub

---
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -0,0 +1,41 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String s = br.readLine();
+        int alpha[] = new int[26];
+
+        for (char x : s.toCharArray()) {
+            alpha[x - 'A']++;
+        }
+        int n = 0, mid = 0;
+        for (int i = 0; i < alpha.length; i++) {
+            if (alpha[i] % 2 == 1) {
+                mid = i;
+                n++;
+            }
+        }
+        if ((s.length() % 2 == 1 && n > 1) || (s.length() % 2 == 0 && n > 0)) {
+            sb.append("I'm Sorry Hansoo");
+        } else {
+            String answer = "";
+            for (int i = 0; i < alpha.length; i++) {
+                for (int j = 0; j < alpha[i] / 2; j++) {
+                    answer += (char) (i + 'A');
+                }
+            }
+            String reverse = new StringBuilder(answer).reverse().toString();
+            if (n == 1) answer += (char) (mid + 'A');
+            sb.append(answer + reverse);
+        }
+        System.out.println(sb);
+
+    }
+}
+

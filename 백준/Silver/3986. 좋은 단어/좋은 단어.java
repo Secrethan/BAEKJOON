b0924fbcc88bccd02577309738116ec3ec0ac10e
Secrethan <107679440+Secrethan@users.noreply.github.com>
2023-09-25 23:03:41 +0900

    [Silver IV] Title: 좋은 단어, Time: 384 ms, Memory: 22872 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.java
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -6,30 +6,36 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int number = Integer.parseInt(br.readLine());
         String str[] = new String[number];
-
+        
         for(int i = 0; i < number; i++) {
-            str[i] = br.readLine();
+            str[i] = br.readLine();     
         }
-
+        
         Stack<Character> stack = new Stack<Character>();
         int goodWords = 0;
+        
+        
 
         for(int i=0; i<number; i++) {
             for(int j=0; j< str[i].length(); j++) {
                 if(!stack.isEmpty() && str[i].charAt(j) == stack.peek())
-                    stack.pop();
+                    stack.pop();	
                 else
-                    stack.push(str[i].charAt(j));
+                    stack.push(str[i].charAt(j));	
             }
-
+       
             if(stack.size() == 0)
                 goodWords ++;
 
             stack.clear();
         }
+
         System.out.println(goodWords);
+        scan.close();
+
     }
 }
